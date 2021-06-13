ab839d572035f2becc7d2acbc975f8c813cc79d6
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2021-06-13 15:57:24 -0400

    added hotel.js perk.js

---
+++ hotels/src/Hotels/Perk.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Perk = (props) => {
+    if (props.hasPerk) {
+        return <span className ="htoel__perks">{props.perk</span>
+    } else {
+        return null
+    }
+}
